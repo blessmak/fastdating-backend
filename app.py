fe55d10f9c2721572996ea5fec1768d584e4acee
blessmak <121347828+blessmak@users.noreply.github.com>
2025-07-11 19:17:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -72,5 +72,8 @@ def like():
 def get_matches():
     return jsonify(matches)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
